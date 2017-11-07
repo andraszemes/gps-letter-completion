3da076329466e5eeef3d493bfead21fc3731f68c
Andras <zemes.andras@gmail.com>
2017-11-07 19:32:02 +0100

    Converted indentation to spaces.

--- app.cpp
+++ app.cpp
@@ -4,44 +4,44 @@
 
 int main(int argc, char *argv[])
 {
-	// Load search string
-	const std::string search = argv[1] ? argv[1] : std::string();
+    // Load search string
+    const std::string search = argv[1] ? argv[1] : std::string();
 
-	// Initialize variables
-	std::string place, res, msg;
+    // Initialize variables
+    std::string place, res, msg;
 
-	// Parse through stdin places
-	while(std::getline(std::cin, place)) 
-	{
-		// If match found, append following character to result string
-		if(!place.rfind(search, 0))
-        	res += place[search.size()];
+    // Parse through stdin places
+    while(std::getline(std::cin, place)) 
+    {
+        // If match found, append following character to result string
+        if(!place.rfind(search, 0))
+            res += place[search.size()];
     }
 
     // Sort characters alphabetically
     std::sort(res.begin(), res.end());
 
     // Transform characters to uppercase
-	transform(res.begin(), res.end(), res.begin(), ::toupper);
+    transform(res.begin(), res.end(), res.begin(), ::toupper);
 
-	// Determine feedback message
-	switch(res.size())
-	{
-		case 0:
-			msg = "Not found";
-			break;
+    // Determine feedback message
+    switch(res.size())
+    {
+        case 0:
+            msg = "Not found";
+            break;
  
-		case 1:
-			msg = "Found: ";
-			break;
+        case 1:
+            msg = "Found: ";
+            break;
 
-		default:
-			msg = "Enable: ";
-	}
+        default:
+            msg = "Enable: ";
+    }
 
-	// Remove duplicate characters from result
-	res.erase(std::unique(res.begin(), res.end()), res.end());
+    // Remove duplicate characters from result
+    res.erase(std::unique(res.begin(), res.end()), res.end());
 
-	// Print result
-	std::cout << msg << res;
+    // Print result
+    std::cout << msg << res;
 }
